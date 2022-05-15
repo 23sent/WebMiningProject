aef1a93485fcf20866de3adb360964d11067eb0b
23sent <buadalinmadi@gmail.com>
2022-05-15 14:48:12 +0300

    Started generating url attribtees 2.

---
+++ Website.py
@@ -0,0 +1,16 @@
+class Website():
+    def __init__(self, url="http://example.com", isBlog=0, isWordpress=0, isBlogSpot=0, isUrlIncludeDateTime=0, isUrlIncludeDomainPostTitle=0, isUrlIncludeYearMonthPostTitlifehacker=0, isUrlIncludeDomainPostTitleNumbers=0, urlLen=0, numOfBlogKeyword=0, numOfKeywords=0, isPrefixBlog=0, isMetaTagIncludeBlogKeyword=0, isUrlSuffixHTML=0):
+        self.url = url
+        self.isBlog = isBlog
+        self.isWordpress = isWordpress
+        self.isBlogSpot = isBlogSpot
+        self.isUrlIncludeDateTime = isUrlIncludeDateTime
+        self.isUrlIncludeDomainPostTitle = isUrlIncludeDomainPostTitle
+        self.isUrlIncludeYearMonthPostTitlifehacker = isUrlIncludeYearMonthPostTitlifehacker
+        self.isUrlIncludeDomainPostTitleNumbers = isUrlIncludeDomainPostTitleNumbers
+        self.urlLen = urlLen
+        self.numOfBlogKeyword = numOfBlogKeyword
+        self.numOfKeywords = numOfKeywords
+        self.isPrefixBlog = isPrefixBlog
+        self.isMetaTagIncludeBlogKeyword = isMetaTagIncludeBlogKeyword
+        self.isUrlSuffixHTML = isUrlSuffixHTML
